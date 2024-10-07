40f1c4e6562e57c6c078ea0012eac654cc3ed516
35e <21952854+35e@users.noreply.github.com>
2024-10-07 13:26:53 +0200

    added option to save a video on infuse

--- src/open-infuse.ts
+++ src/open-infuse.ts
@@ -1,6 +1,12 @@
 import { open } from "@raycast/api";
 
-export default async function main(props: { arguments: { url: string } }) {
-  const { url } = props.arguments;
-  await open(`infuse://x-callback-url/play?url=${url}`);
+export default async function main(props: { arguments: { url: string, action: string } }) {
+  const { url, action } = props.arguments;
+  const encodedUrl = encodeURIComponent(url);
+  
+  if (action === "save") {
+    await open(`infuse://x-callback-url/save?url=${encodedUrl}`);
+  } else if (action === "play") {
+    await open(`infuse://x-callback-url/play?url=${encodedUrl}`);
+  }
 }
